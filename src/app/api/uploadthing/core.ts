d8332418be7af6f7f3b4cf4b3951e086c236f934
Quan Zheng <q.zheng-7@student.tudelft.nl>
2024-04-24 22:36:50 -0700

    final commit

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { db } from "~/server/db";
@@ -19,8 +19,12 @@ export const ourFileRouter = {
       // If you throw, the user will not be able to upload
       if (!user.userId) throw new UploadThingError("Unauthorized");
 
-      const { success } = await ratelimit.limit(user.userId);
+      const fullUserData = await clerkClient.users.getUser(user.userId);
 
+      if (fullUserData?.privateMetadata?.["can-upload"] !== "true")
+        throw new UploadThingError("User does not have upload permissions");
+
+      const { success } = await ratelimit.limit(user.userId);
       if (!success) throw new UploadThingError("Rate limited");
 
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
